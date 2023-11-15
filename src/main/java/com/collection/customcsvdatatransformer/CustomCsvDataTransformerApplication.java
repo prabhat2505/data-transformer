2e4b3616446dc08a2ae296dfcbb91d0b408a81a0
Prabhat Rana <prabhat.rana828@gmail.com>
2023-11-15 13:46:00 +0530

    Initializing Project

---
+++ src/main/java/com/collection/customcsvdatatransformer/CustomCsvDataTransformerApplication.java
@@ -0,0 +1,13 @@
+package com.collection.customcsvdatatransformer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomCsvDataTransformerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomCsvDataTransformerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collection/customcsvdatatransformer/CustomCsvDataTransformerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collection.customcsvdatatransformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomCsvDataTransformerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
